cec4b854b9a1f5a63209b17f02a5fe18af8efa5b
Michal Orzechowski <michal.orzechowski@ig.com>
2020-11-18 11:33:03 +0100

    Add JsonPlaceHolder base class

--- src/test/java/apittask/BaseTest.java
+++ src/test/java/apittask/BaseTest.java
@@ -1,5 +1,6 @@
 package apittask;
 
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
@@ -7,12 +8,12 @@ import io.restassured.specification.RequestSpecification;
 public abstract class BaseTest {
 
    private static final String URL = "https://jsonplaceholder.typicode.com/";
-   public static RequestSpecification REQUEST_SPEC= new RequestSpecBuilder().setBaseUri(URL).setContentType(ContentType.JSON).build();
+   public static RequestSpecification REQUEST_SPEC = new RequestSpecBuilder().setBaseUri(URL).setContentType(ContentType.JSON).build();
 
    public RequestSpecification given() {
-      return io.restassured.RestAssured.given()
-                                       .spec(REQUEST_SPEC)
-                                       .log().all();
+      return RestAssured.given()
+                        .spec(REQUEST_SPEC)
+                        .log().all();
 
    }
 }
---
+++ src/test/java/javatask/TaskTests.java
@@ -0,0 +1,7 @@
+package javatask;
+
+import org.junit.jupiter.api.Test;
+
+public class TaskTests {
+
+}
