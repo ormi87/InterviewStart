b30ba2b356bb86f7f862ced6643fb35f2a8fb242
Michal Orzechowski <michal.orzechowski@ig.com>
2020-11-18 11:23:23 +0100

    Add JsonPlaceHolder base class

--- src/test/java/javatask/InitClass.java
+++ src/test/java/javatask/InitClass.java
@@ -2,7 +2,18 @@ package javatask;
 
 public class InitClass {
 
+    /**
+     * Given an array of integers X check if adding 1 to any of the elements in array would make it possible to retrieve this element value
+     * as a result of summing any other two elements in array.
+     *
+     * Example:
+     * X={1,3,7,3,5} -> retun true, because for X[4]+1==X[1]+X[3]: 5+1==3+3
+     * X={1,4,10}    -> return false
+     * @param args
+     */
+
+
     public static void main(String []args) {
-        System.out.println("Hello World");
+
     }
 }
